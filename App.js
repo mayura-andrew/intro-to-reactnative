1795ce35781c386e6ecfc20bda0b0704c2fa0b49
mayura-andrew <mayuraalahakoon@gmail.com>
2023-08-15 13:19:33 +0530

    styles commopents

--- App.js
+++ App.js
@@ -3,20 +3,25 @@ import { StyleSheet,Text, View, SafeAreaView } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView style = {styles.SafeArea}>
+    <SafeAreaView>
     <View style ={styles.container}>
+      <Text style = {styles.text}>Working on something new:) Let's goooo</Text>
+    </View>
+    <View style ={styles.container}>
+      <Text style={styles.heading}>Here are some boxex of different colors</Text>
+    <View style ={[styles.box, styles.cyanbox]}>
       <Text>Working on something new:) Let's goooo</Text>
     </View>
-    <View style ={styles.container1}>
+    <View style ={[styles.box, styles.blueBox]}>
       <Text>Working on something new:) Let's goooo</Text>
     </View>
-    <View style ={styles.container2}>
+    <View style ={[styles.box, styles.magenta]}>
       <Text>Working on something new:) Let's goooo</Text>
     </View>
-    <View style ={styles.container3}>
+    <View style ={[styles.box, styles.orange]}>
       <Text>Working on something new:) Let's goooo</Text>
     </View>
-
+    </View>
     </SafeAreaView>
   );
  
@@ -24,74 +29,39 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    // marginTop: 50,
-    // paddingTop: 50, 
-    // paddingBottom: 50,
-    // paddingLeft:30,
-    // paddingRight:30,
-
-    paddingVertical: 2,
-    paddingHorizontal: 2,
-    marginVertical: 1,
-    marginHorizontal: 1,
-    backgroundColor: 'cyan',
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+  heading:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  box:{
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
+    marginBottom: 10,
   },
-  container1: {
-    // marginTop: 50,
-    // paddingTop: 50, 
-    // paddingBottom: 50,
-    // paddingLeft:30,
-    // paddingRight:30,
-
-    paddingVertical: 30,
-    paddingHorizontal: 30,
-    marginVertical: 10,
-    marginHorizontal: 10,
-    backgroundColor: 'red',
-    alignItems: 'blue',
-    justifyContent: 'center',
-    flex: 1,
+  cyanbox:{
+    backgroundColor: '#2aa198',
   },
-  container2: {
-    // marginTop: 50,
-    // paddingTop: 50, 
-    // paddingBottom: 50,
-    // paddingLeft:30,
-    // paddingRight:30,
-
-    paddingVertical: 30,
-    paddingHorizontal: 30,
-    marginVertical: 10,
-    marginHorizontal: 10,
-    backgroundColor: 'magenta',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
+  blueBox:{
+    backgroundColor: '#268bd2',
   },
-  container3: {
-    // marginTop: 50,
-    // paddingTop: 50, 
-    // paddingBottom: 50,
-    // paddingLeft:30,
-    // paddingRight:30,
-    marginVertical: 10,
-    marginHorizontal: 10,
-    paddingVertical: 30,
-    paddingHorizontal: 30,
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
     backgroundColor: '#cb4b16',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-  },
-  SafeArea:{
-    flex:1,
-  }
-  
-
-});
+  }}
+);
 
 export default App;
 
