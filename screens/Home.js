d14d2e2a4847da3c532ab3858f887308bc0ebd90
mayura-andrew <mayuraalahakoon@gmail.com>
2023-08-16 13:46:36 +0530

    added refresh option

--- screens/Home.js
+++ screens/Home.js
@@ -1,65 +1,59 @@
-import react, {useState, useEffect, useCallback} from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import { FlatList, RefreshControl } from "react-native";
-import PalettePreview  from "../components/PalettePreview";
-
+import React, { useState, useEffect, useCallback } from "react";
+import { StyleSheet, FlatList, RefreshControl } from "react-native";
+import PalettePreview from "../components/PalettePreview";
 
 const URL = 'https://color-palette-api.kadikraman.now.sh/palettes';
 
-
-
-
-const Home = ({navigation}) => {
-
+const Home = ({ navigation }) => {
     const [palettes, setPalettes] = useState([]);
     const [isRefreshing, setIsRefreshing] = useState(false);
 
     const handleFetchPalettes = useCallback(async () => {
-        const response = await fetch(URL);
-        if(response.ok){
-            const palettes = await response.json();
-            console.log(palettes);
-            setPalettes(palettes);
+        try {
+            const response = await fetch(URL);
+            if (response.ok) {
+                const palettes = await response.json();
+                setPalettes(palettes);
+            }
+        } catch (error) {
+            console.error("Error fetching palettes:", error);
         }
-}, []);
+    }, []);
 
-    useEffect(() =>{
+    useEffect(() => {
         handleFetchPalettes();
     }, []);
 
-
     const handleRefresh = useCallback(async () => {
         setIsRefreshing(true);
-        await handleFetchPalettes();
-        setTimeout(() => {
-            setIsRefreshing(false);
-        }, 1000);
-    })
+        await handleFetchPalettes(); // Fetch new data
+        setIsRefreshing(false); // Done refreshing
+    }, [handleFetchPalettes]);
+
     return (
         <FlatList
-        style={styles.list}
-        data={palettes}
-        keyExtractor={item => item.paletteName}
-        renderItem={({item}) => (
-            <PalettePreview
-            onPress={() => navigation.push('ColorPalette', item)}
-            palette={item}
-            />
+            style={styles.list}
+            data={palettes}
+            keyExtractor={item => item.paletteName}
+            renderItem={({ item }) => (
+                <PalettePreview
+                    onPress={() => navigation.push('ColorPalette', item)}
+                    palette={item}
+                />
             )}
             refreshControl={
                 <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
             }
-            />
+        />
     );
 };
 
-
 const styles = StyleSheet.create({
-    list:{
-        flex:1,
-        padding:10,
-        backgroundColor:'white',
-    }
+    list: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: 'white',
+    },
 });
 
 export default Home;
